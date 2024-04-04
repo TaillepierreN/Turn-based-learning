d3d8d2dac9178b0dd0d3c7e17d216039f0c84f78
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-04 14:56:46 +0200

    Added unit selected visuals using event handling

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
 	[SerializeField]private Unit selectedUnit;
 	[SerializeField] private LayerMask UnitLayerMask;
+	public event EventHandler OnSelectedUnitChanged;
+	public static UnitActionSystem Instance { get; private set;}
+
+	private void Awake() {
+		if (Instance != null)
+		{
+			Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
+			Destroy(gameObject);
+			return;
+		}
+		Instance = this;
+	}
 
 	private void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
 			if (TryHandleUnitSelection()) return;
+			
 			selectedUnit.Move(MouseTracking.GetPosition());
 		}
 	}
@@ -23,10 +37,25 @@ public class UnitActionSystem : MonoBehaviour
 		{
 			if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
 			{
-				selectedUnit = unit;
+				SetSelectedUnit(unit);
 				return true;
 			}
 		}
 		return false;
+	}
+
+	private void SetSelectedUnit(Unit unit)
+	{
+		selectedUnit = unit;
+		if (OnSelectedUnitChanged != null)
+		{
+		OnSelectedUnitChanged(this, EventArgs.Empty);
+		}
+		//OnSelectedUnitChanger?.Invoke(this, EventArgs.Empty) does the same
+	}
+
+	public Unit GetSelectedUnit()
+	{
+		return selectedUnit;
 	}
 }
